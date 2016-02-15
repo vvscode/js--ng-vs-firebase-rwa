786eae3fcda0487e3c28f66a7c9a1ac1a9fc064c
Vasil Vanchuk <v.vanchuk@tut.by>
2016-02-15 21:43:37 +0300

    Setup mdThemingProvider

--- scripts/app.js
+++ scripts/app.js
@@ -1,2 +1,9 @@
-angular.module('rw', []);
+angular
+  .module('rw', ['ngMaterial'])
+  .config(function($mdThemingProvider) {
+    $mdThemingProvider
+      .theme('default')
+      .primaryPalette('teal')
+      .accentPalette('orange');
+  });
 
