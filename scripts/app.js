d4668c4cf18fcb4c2c08efa32eb66c6718a28e7b
Vasil Vanchuk <v.vanchuk@tut.by>
2016-02-16 22:45:32 +0300

    Primitive directive

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,16 @@
 angular
   .module('rw', ['ngMaterial'])
   .config(function($mdThemingProvider) {
+
     $mdThemingProvider
       .theme('default')
       .primaryPalette('teal')
       .accentPalette('orange');
+
+  })
+  .directive('helloWorld', function() {
+    return {
+      template: "<h1>Hello, World</h1>"
+    };
   });
 
