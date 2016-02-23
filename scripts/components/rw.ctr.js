83b2b7039cc5c43235d83bd2ab1e52192d632410
Vasil Vanchuk <v.vanchuk@tut.by>
2016-02-23 23:11:26 +0300

    Add toggled sidebar with item form

--- scripts/components/rw.ctr.js
+++ scripts/components/rw.ctr.js
@@ -3,7 +3,10 @@
 
   angular
     .module('rw')
-    .controller('RwCtrl', function(rwFactory) {
+    .controller('RwCtrl', function(rwFactory, $mdSidenav) {
       rwFactory.getItems().then((data) => (this.items = data.data));
+
+      this.openSidebar = () => $mdSidenav('left').open();
+      this.closeSidebar = () => $mdSidenav('left').close();
     });
 })();
