2aaab819c10f3c72b7427ae5d843e0e72cb22bc9
Vasil Vanchuk <v.vanchuk@tut.by>
2016-02-28 21:00:10 +0300

    Add toast on add new Item

--- scripts/components/rw.ctr.js
+++ scripts/components/rw.ctr.js
@@ -3,15 +3,23 @@
 
   angular
     .module('rw')
-    .controller('RwCtrl', function(rwFactory, $mdSidenav) {
+    .controller('RwCtrl', function(rwFactory, $mdSidenav, $mdToast) {
       rwFactory.getItems().then((data) => (this.items = data.data));
 
       this.openSidebar = () => $mdSidenav('left').open();
       this.closeSidebar = () => $mdSidenav('left').close();
+      this.editItem = (item) => { };
+      this.removeItem = (item) => { }
       this.saveItem = (item) => {
         this.items.push(item);
         this.item = {};
         this.closeSidebar();
+        $mdToast.show(
+          $mdToast.simple()
+            .content('Item saved')
+            .position('top, right')
+            .hideDelay(3000)
+        );
       };
       this.saveEdit = () => {};
     });
